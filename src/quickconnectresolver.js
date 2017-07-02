0a944943a88e90a757102a96fec8510bc4caba7f
Thomas Theunen <thomas.theunen@fenego.be>
2017-07-02 15:27:08 +0200

    Initial commit

---
+++ src/quickconnectresolver.js
@@ -0,0 +1,108 @@
+var QuickConnect = function(id) {
+    var quickConnectID = id;
+    var requestQueue = [];
+
+    function determineServerURL(success, fail) {
+        getServerData(function(response) {
+            if (response[0].server && response[0].service) {
+                createCallDSMDirectlyRequests(response[0]);
+
+
+                processRequestQueue(function(url) {
+                    success(url);
+                }, function(error) {
+
+                });
+            } else {
+                if (fail)
+                    fail("No server found");
+            }
+        });
+    };
+
+    function processRequestQueue(success, error) {
+        for (var i = 0; i < requestQueue.length; i++) {
+            var request = requestQueue[i];
+            request.xhr.ip = request.ip
+            request.xhr.port = request.port;
+            request.xhr.onload = function() {
+                if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                    success('https://' + this.ip + ':' + this.port);
+                }
+            }
+
+            request.xhr.send(null);
+
+        }
+    }
+
+    function getServerData(done) {
+        var serverRequestData = [{
+            "version": 1,
+            "command": "get_server_info",
+            "stop_when_error": "false",
+            "stop_when_success": "false",
+            "id": "dsm_portal_https",
+            "serverID": quickConnectID
+        }];
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://synologyquickconnectid.herokuapp.com/server.php', true);
+
+        xhr.onload = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                var serversResponse = JSON.parse(xhr.responseText);
+                done(serversResponse);
+            }
+        };
+
+        xhr.send(JSON.stringify(serverRequestData));
+
+        return xhr;
+    }
+
+    function createCallDSMDirectlyRequests(serverData) {
+        var port = serverData.service.port;
+        var externalPort = serverData.service.ext_port;
+
+        if (serverData.server.interface) {
+            for (var i = 0; i < serverData.server.interface.length; i++) {
+                var serverInterface = serverData.server.interface[i];
+
+                if (serverInterface.ip) {
+                    var pingPong = createPingPongCall(serverInterface.ip, port);
+                    requestQueue.push({
+                        xhr: pingPong,
+                        ip: serverInterface.ip,
+                        port: port
+                    });
+                }
+
+                if (serverInterface.ipv6 && serverInterface.ipv6.length > 0) {
+                    for (var j = 0; j < serverInterface.ipv6.length; j++) {
+
+                        var ipv6 = serverInterface.ipv6[i];
+                        var pingPong = createPingPongCall('[' + ipv6.address + ']', port);
+                        requestQueue.push({
+                            xhr: pingPong,
+                            ip: '[' + ipv6.address + ']',
+                            port: port
+                        });
+                    }
+                }
+
+            }
+        }
+    }
+
+    function createPingPongCall(ip, port) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://' + ip + ":" + port + "/webman/pingpong.cgi?action=cors", true);
+
+        return xhr;
+    }
+
+    return {
+        "determineServerURL": determineServerURL
+    }
+}
